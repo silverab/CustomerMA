66d6e0d431d2b853935bfcc2ca4cd2b0596c8d22
Hassan Fiaz <68145207+silverab@users.noreply.github.com>
2022-07-23 14:26:31 +0500

    Persmissions & Authentications

--- accounts/models.py
+++ accounts/models.py
@@ -11,43 +11,45 @@ class Customer(models.Model):
 	def __str__(self):
 		return self.name
 
+
 class Tag(models.Model):
 	name = models.CharField(max_length=200, null=True)
 
 	def __str__(self):
 		return self.name
 
-
 class Product(models.Model):
 	CATEGORY = (
-				('Indoor', 'Indoor'),
-				('Out door', 'Out door'),
-				)
+			('Indoor', 'Indoor'),
+			('Out Door', 'Out Door'),
+			) 
+
 	name = models.CharField(max_length=200, null=True)
 	price = models.FloatField(null=True)
 	category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-	description =  models.CharField(max_length=200, null=True, blank=True)
+	description = models.CharField(max_length=200, null=True, blank=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
-	tags =  models.ManyToManyField(Tag)
+	tags = models.ManyToManyField(Tag)
 
 	def __str__(self):
 		return self.name
 
-
-
 class Order(models.Model):
-
 	STATUS = (
-			('Pending','Pending'),
+			('Pending', 'Pending'),
 			('Out for delivery', 'Out for delivery'),
 			('Delivered', 'Delivered'),
 			)
 
-	customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
-	product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL  )
+	customer = models.ForeignKey(Customer, null=True, on_delete= models.SET_NULL)
+	product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 	status = models.CharField(max_length=200, null=True, choices=STATUS)
 	note = models.CharField(max_length=1000, null=True)
 
 	def __str__(self):
 		return self.product.name
+
+
+
+	
